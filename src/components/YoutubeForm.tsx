e71877af0f3535d6356f520f2b203c005c75f67a
Saeed <s8990@yahoo.com>
2024-07-31 20:24:48 +0330

    using register method in simplest way

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -3,7 +3,6 @@ import { useForm } from "react-hook-form";
 export const YouTubeForm = () => {
   const form = useForm();
   const { register } = form;
-  const { name, ref, onChange, onBlur } = register("username");
 
   return (
     <div>
@@ -11,13 +10,13 @@ export const YouTubeForm = () => {
 
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name={name} ref={ref} onChange={onChange} onBlur={onBlur} />
+        <input type="text" id="username" {...register("username")} />
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" {...register("email")} />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input type="text" id="channel" {...register("channel")} />
 
         <button>Submit</button>
       </form>
