1dc6d3ec0ec4ad08c8462eea17c27a64d59ff7b9
Saeed <s8990@yahoo.com>
2024-07-31 20:16:53 +0330

    add youtube form - simple html

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { YouTubeForm } from './components/YoutubeForm'
+
+function App() {
+
+  return (
+    <div>
+      <YouTubeForm />
+    </div>
+  )
+}
+
+export default App
