f6bb7a8b850c473828d971783e8ca76a001ccb00
Saeed <s8990@yahoo.com>
2024-07-31 20:47:36 +0330

    add some validators to form component

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -2,9 +2,9 @@ import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
 interface FormValues {
-    username: string;
-    email: string;
-    channel: string;
+  username: string;
+  email: string;
+  channel: string;
 }
 
 export const YouTubeForm = () => {
@@ -19,15 +19,33 @@ export const YouTubeForm = () => {
     <div>
       <h1>YouTube Form</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form noValidate onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" {...register("username")} />
+        <input
+          type="text"
+          id="username"
+          {...register("username", {
+            required: "username is required",
+          })}
+        />
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" {...register("email")} />
+        <input type="email" id="email" {...register("email", {
+            required: "email is required",
+            pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                message: "invalid format",
+            },
+        })} />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register("channel")} />
+        <input
+          type="text"
+          id="channel"
+          {...register("channel", {
+            required: "channel name is required",
+          })}
+        />
 
         <button>Submit</button>
       </form>
